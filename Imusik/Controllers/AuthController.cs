57d593422a4bc13b47aae8fd43198e145a853a06
Nguyễn Sơn <sonhoai272@gmail.com>
2018-04-30 00:11:51 +0700

    jwt

---
+++ Imusik/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Imusik.Controllers
+{
+    public class AuthController : ApiController
+    {
+        [HttpPost]
+        public string Token()
+        {
+            return JwtManager.GenerateToken("AAAA");
+        }
+    }
+}
